f2f0a7a2bf0e8ac05be5d208c8f40dd806aa98e9
owokomoses <owokomose@gmail.com>
2023-05-23 14:30:12 +0300

    added files

--- app/src/main/java/com/owoko/androidintro/MainActivity.kt
+++ app/src/main/java/com/owoko/androidintro/MainActivity.kt
@@ -1,11 +1,22 @@
 package com.owoko.androidintro
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
+    lateinit var button:Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        button=findViewById(R.id.button)
+
+        button.setOnClickListener {
+            val intent=Intent(this,animeactivity::class.java)
+            startActivity(intent)
+        }
+
+        }
+
     }
-}
---
+++ app/src/main/java/com/owoko/androidintro/animeactivity.kt
@@ -0,0 +1,11 @@
+package com.owoko.androidintro
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class animeactivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_animeactivity)
+    }
+}
